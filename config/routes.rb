70102a1ac2c73cd755ddfa12ba6909a5c7f1395d
Laguna1 <lagunazoomarket@gmail.com>
2020-10-23 00:28:57 +0300

    add articles actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get 'about', to: 'pages#about'
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
