3d1acc86f252be9325b577480d49361f24c2dc14
Laguna1 <lagunazoomarket@gmail.com>
2020-10-23 03:01:14 +0300

    add password_digest to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
-    has_many :articles
-    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
-    validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, 
-    format: { with: VALID_EMAIL_REGEX }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    has_many :articles, dependent: :destroy
     before_save { self.email = email.downcase }
-   end
+    validates :username, presence: true, 
+              uniqueness: { case_sensitive: false }, 
+              length: { minimum: 3, maximum: 25 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 105 },
+              uniqueness: { case_sensitive: false },
+              format: { with: VALID_EMAIL_REGEX }
+    has_secure_password
+  end
