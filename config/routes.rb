7908dd7d1bc4e0c1a48ec176e5ea61ad122c332d
Laguna1 <lagunazoomarket@gmail.com>
2020-10-23 03:34:02 +0300

    add users views pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'pages#index'
-  get 'about', to: 'pages#about'
   resources :articles
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
